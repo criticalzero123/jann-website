fb7344fe5289e3fa08528fd8da455b9486f40bef
unknown <jdcaramonte159@gmail.com>
2020-07-14 23:13:00 +0800

    changing to peso sign

--- src/components/cart-item/cart-item.component.js
+++ src/components/cart-item/cart-item.component.js
@@ -8,7 +8,7 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     <div className="item-details">
       <span className="name">{name}</span>
       <span className="price">
-        {quantity} x ${price}
+        {quantity} x &#8369;{price}
       </span>
     </div>
   </div>
