7ef10019cc38df4a09b35b558130588d2ccb2b91
unknown <jdcaramonte159@gmail.com>
2020-07-14 23:08:53 +0800

    changing to peso sign

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -34,7 +34,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <span>TOTAL: ${total}</span>
+      <span>TOTAL: &#8369;{total}</span>
     </div>
   </div>
 );
